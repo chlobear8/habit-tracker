6dcd0fcd5ec0b758de5c22c220d5d47d2afef8c5
Chloe ONeil <chloesporri@yahoo.com>
2024-04-01 11:28:11 -0700

    add useState and onChange event to TrackerForm

--- src/components/TrackerForm.js
+++ src/components/TrackerForm.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const TrackerForm = () => {
+export const TrackerForm = () => {
+  const [value, setValue] = useState("");
   return (
     <form className='TrackerForm'>
-      <input type='text' className='tracker-input' placeholder='What habit would you like to track?'/>
+      <input type='text' className='tracker-input' placeholder='What habit would you like to track?' onChange={(e) => console.log(e.target.value)}/>
       <button type='submit' className='tracker-btn'>Add Habit</button>
     </form>
   )
