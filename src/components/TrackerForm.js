6bef2996da43ba9973de1c1f2bdbc7bb753c2249
Chloe ONeil <chloesporri@yahoo.com>
2024-04-01 11:37:40 -0700

    add handleSubmit function

--- src/components/TrackerForm.js
+++ src/components/TrackerForm.js
@@ -2,9 +2,14 @@ import React, { useState } from 'react';
 
 export const TrackerForm = () => {
   const [value, setValue] = useState("");
+  
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(value);
+  }
   return (
-    <form className='TrackerForm'>
-      <input type='text' className='tracker-input' placeholder='What habit would you like to track?' onChange={(e) => console.log(e.target.value)}/>
+    <form className='TrackerForm' onSubmit={handleSubmit}>
+      <input type='text' className='tracker-input' placeholder='What habit would you like to track?' onChange={(e) => setValue(e.target.value)}/>
       <button type='submit' className='tracker-btn'>Add Habit</button>
     </form>
   )
