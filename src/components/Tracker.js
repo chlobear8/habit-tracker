ea2f527a5d1eea04b1d6b1e11ce7be0c083ae9c2
Chloe ONeil <chloesporri@yahoo.com>
2024-04-02 17:08:02 -0700

    add icon to Tracker.js

--- src/components/Tracker.js
+++ src/components/Tracker.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 export const Tracker = () => {
   return (
-    <div>
-
+    <div className='Tracker'>
+      <p>Track Your Habits</p>
+      <div>
+        <FontAwesomeIcon icon={faPenToSquare} />
+        <FontAwesomeIcon icon={faTrash} />
+      </div>
     </div>
   )
 };
