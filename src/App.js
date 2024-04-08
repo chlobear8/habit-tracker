abe8296d4be9809dbcbd7e9c431bd00fd5504d2b
Chloe ONeil <chloesporri@yahoo.com>
2024-04-08 10:17:10 -0700

    add Routes to App.js and remove components to replace with Routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import {TrackerWrapper} from './components/TrackerWrapper';
-import { ProgressBar } from './components/ProgressBar';
+import { Routes } from './Routes';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Route exact path="/" component={TrackerWrapper} />
-        <Route path="/progress" component={ProgressBar} />
-      </div>
-    </Router>
+    <Routes />
   );
 }
 
